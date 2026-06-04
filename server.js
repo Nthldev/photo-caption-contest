088a75c22cd19f6533348b4732b0be22e13cc521
Nthldev <nthlevis@gmail.com>
2026-06-04 18:07:20 -0300

    servidor basico criado em server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.send('Bem vindo ao concurso de fotos para legenda')
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor iniciado na porta ${PORT}`)
+});
