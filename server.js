947e09ef990f55779158da98e774816dd95b47a5
Nthldev <nthlevis@gmail.com>
2026-06-06 19:56:24 -0300

    adicionando imagens estaticas e configurado express.static no servidor

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.use(express.static('public'));
+
 app.get('/', (req, res)=>{
     res.send('Bem vindo ao concurso de fotos para legenda')
 });
