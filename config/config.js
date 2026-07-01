716356171ee0b7bcced2d5f63c19f9ad5cad4b86
Nthldev <nthlevis@gmail.com>
2026-07-01 13:01:06 -0300

    adicionado seederstorage do sequelize no ambiente de producao

--- config/config.js
+++ config/config.js
@@ -19,6 +19,7 @@ module.exports = {
   "production": {
     "use_env_variable": "DATABASE_URL",
     "dialect": "postgres",
+    "seederStorage": "sequelize",
     "dialectOptions": {
         "ssl": {
             "require": true,
